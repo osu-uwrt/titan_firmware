3c7d81d4da0bdb9a80135bc1f60dd22fc08b541b
coalman321 <cjtucker321@gmail.com>
2023-01-21 23:45:22 -0500

    a bit closer

--- Backplane/main.c
+++ Backplane/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#include "pico_eth_transport.h"
+#include <types.h>
+
 #include "build_version.h"
 
 int main() {
@@ -14,10 +17,14 @@ int main() {
     gpio_init(LED_RGB_B_PIN);
     gpio_set_dir(LED_RGB_B_PIN, GPIO_OUT);
 
+    // Eth init
+    uint8_t xavier_ip[] = {192, 168, 1, 23};
+    uint16_t xavier_port = 8888;
+    pico_eth_transport_init(0, *((uint32_t*)(&xavier_ip)), xavier_port);
 
     printf("%s\n", FULL_BUILD_TAG);
 
-    unsigned char count = 0;
+    unsigned char count = 7;
 
     while (true) {
         printf("Hello World!\n");
@@ -26,7 +33,7 @@ int main() {
         gpio_put(LED_RGB_G_PIN, count & 0x02);
         gpio_put(LED_RGB_B_PIN, count & 0x04);
 
-        count = count >= 8 ? 0 : count + 1;
+        count = count > 0 ? 7 : count - 1;
         
         sleep_ms(1000);
     }
--- lib/micro_ros_pico/build.sh
+++ lib/micro_ros_pico/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
+
+sudo chmod +x ./microros_static_library/library_generation/library_generation.sh
+
 docker pull microros/micro_ros_static_library_builder:humble
 docker run -it --rm -v $(pwd):/project microros/micro_ros_static_library_builder:humble
