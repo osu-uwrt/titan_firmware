915574a8e2db89023e637185c750a7917840d084
Robert Pafford <19439938+rjp5th@users.noreply.github.com>
2023-03-05 18:30:51 -0500

    Working useless bootloader

--- bootloader/main.c
+++ bootloader/main.c
@@ -1,3 +1,15 @@
+#include "hardware/gpio.h"
+#include "hardware/timer.h"
+
 int main(void) {
-    return 0;
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_put(PICO_DEFAULT_LED_PIN, 0);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+    while(1) {
+        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        busy_wait_ms(500);
+        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        busy_wait_ms(500);
+    }
 }
