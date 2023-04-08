4189ce08e2a3bcdb4eaf349e0e702409f2c51486
coalman321 <cjtucker321@gmail.com>
2023-04-08 12:44:11 -0400

    kinda working SBH mcu over PIO

--- talos/SmartBattery/src/BQ40Z80.c
+++ talos/SmartBattery/src/BQ40Z80.c
@@ -1,6 +1,7 @@
 #include "BQ40Z80.h"
-#include "pio_i2c.h"
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/i2c.h"
 
 #include <stdio.h>
 
@@ -27,44 +28,26 @@ uint8_t BQ_init() {
         gpio_put(BQ_LEDS[led], 0);
     }
 
+    // init I2c peripheral on the pins
+    i2c_init(i2c1, 100000);
+    gpio_set_function(BMS_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(BMS_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(BMS_SDA_PIN);
+    gpio_pull_up(BMS_SCL_PIN);
 
-    uint offset = pio_add_program(pio0, &i2c_program);
-    i2c_program_init(pio0, 0, offset, BMS_SDA_PIN, BMS_SCL_PIN);
 
-    printf("\nPIO I2C Bus Scan\n");
-    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
-
-    for (int addr = 0; addr < (1 << 7); ++addr) {
-        if (addr % 16 == 0) {
-            printf("%02x ", addr);
+    // Start the I2C to the chip
+    while((data[1] == 0x00 || data[1] == 0xFF) && retries < 3) {
+        i2c_write_blocking(i2c1, BQ_ADDR, data, 1, false);
+        i2c_read_blocking(i2c1, BQ_ADDR, data, 2, false);
+        // Check for valid data
+        if(data[1] == 0x00 || data[1] == 0xFF) {
+            gpio_put(BMS_WAKE_PIN, 1);
+            sleep_ms(1000);
+            gpio_put(BMS_WAKE_PIN, 0);
+            retries++;
         }
-        // Perform a 0-byte read from the probe address. The read function
-        // returns a negative result NAK'd any time other than the last data
-        // byte. Skip over reserved addresses.
-        int result;
-        if (reserved_addr(addr))
-            result = -1;
-        else
-            result = pio_i2c_read_blocking(pio0, 0, addr, NULL, 0);
-
-        printf(result < 0 ? "." : "@");
-        printf(addr % 16 == 15 ? "\n" : "  ");
     }
-    printf("Done.\n");
-    panic("BRRRRR");
-
-    // Start Roberts Jank I2C implementation
-    // while((data[1] == 0x00 || data[1] == 0xFF) && retries < 3) {
-    //     pio_i2c_write_blocking(pio0, 0, BQ_ADDR, data, 1);
-    //     pio_i2c_read_blocking(pio0, 0, BQ_ADDR, data, 2);
-    //     // Check for valid data
-    //     if(data[1] == 0x00 || data[1] == 0xFF) {
-    //         gpio_put(BMS_WAKE_PIN, 1);
-    //         sleep_ms(1000);
-    //         gpio_put(BMS_WAKE_PIN, 0);
-    //         retries++;
-    //     }
-    // }
     return retries;
 }
 
