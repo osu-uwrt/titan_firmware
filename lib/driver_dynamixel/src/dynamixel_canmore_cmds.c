b92a8cc093cb584a22e1fdae72729b52a8d9372d
Robert Pafford <19439938+rjp5th@users.noreply.github.com>
2024-04-10 18:57:29 -0400

    Better dynamixel debug info

--- lib/driver_dynamixel/src/dynamixel_canmore_cmds.c
+++ lib/driver_dynamixel/src/dynamixel_canmore_cmds.c
@@ -95,6 +95,11 @@ static int dynwrite_cmd_cb(size_t argc, const char *const *argv, FILE *fout) {
     return 0;
 }
 
+#define str_table_lookup(table, idx) ((idx) < (sizeof(table) / sizeof(*table)) ? table[idx] : "Unknown")
+#define bool_lookup(val) ((val) ? "Yes" : "No")
+
+static const char *const velocity_profiles[] = { "Not Used", "Rectangular", "Triangular", "Trapezoidal" };
+
 static int dynram_cmd_cb(size_t argc, const char *const *argv, FILE *fout) {
     if (argc < 2) {
         fprintf(fout, "Usage: dynram [id]\n");
@@ -111,18 +116,55 @@ static int dynram_cmd_cb(size_t argc, const char *const *argv, FILE *fout) {
     }
 
     fprintf(fout, "--- Servo %d RAM ---\n", id);
-    fprintf(fout, "  Torque Enable: %d\n", ram->torque_enable);
-    fprintf(fout, "  Moving: %d\n", ram->moving);
-    fprintf(fout, "  Realtime Tick: %d\n", ram->realtime_tick);
-    fprintf(fout, "  Hardware Error Status: %d\n", ram->hardware_error_status);
-    fprintf(fout, "  Goal Position: %ld\n", ram->goal_position);
-    fprintf(fout, "  Present Position: %ld\n", ram->present_position);
-    fprintf(fout, "  Present Velocity: %ld\n", ram->present_velocity);
-    fprintf(fout, "  Present Voltage: %d.%d V\n", ram->present_input_voltage / 10, ram->present_input_voltage % 10);
-    fprintf(fout, "  Present Temperature: %d C\n", ram->present_temperature);
+    fprintf(fout, "  Torque Enable (64):\t\t%s\n", bool_lookup(ram->torque_enable));
+    fprintf(fout, "  Moving (122):\t\t\t%s\n", bool_lookup(ram->moving));
+    fprintf(fout, "  Moving Status (123):\t\t%d\n", ram->moving_status);
+    fprintf(fout, "   - Bit 0: In-Position:\t%s\n", bool_lookup(ram->moving_status & (1 << 0)));
+    fprintf(fout, "   - Bit 1: Profile Ongoing:\t%s\n", bool_lookup(ram->moving_status & (1 << 1)));
+    fprintf(fout, "   - Bit 3: Following Error:\t%s\n", bool_lookup(ram->moving_status & (1 << 3)));
+    fprintf(fout, "   - Bit 4-5: Velocity Profile:\t%s\n",
+            str_table_lookup(velocity_profiles, (ram->moving_status >> 4) & 0x3));
+    fprintf(fout, "  Realtime Tick (120):\t\t%d ms\n", ram->realtime_tick);
+
+    fprintf(fout, "  Hardware Error Status (70):\t%d\n", ram->hardware_error_status);
+    if (ram->hardware_error_status & (1 << 0))
+        fprintf(fout, "    - Bit 0: Input Voltage Error\n");
+    if (ram->hardware_error_status & (1 << 1))
+        fprintf(fout, "    - Bit 1: Unknown???\n");
+    if (ram->hardware_error_status & (1 << 2))
+        fprintf(fout, "    - Bit 2: Overheating Error\n");
+    if (ram->hardware_error_status & (1 << 3))
+        fprintf(fout, "    - Bit 3: Motor Encoder Error\n");
+    if (ram->hardware_error_status & (1 << 4))
+        fprintf(fout, "    - Bit 4: Electrical Shock Error\n");
+    if (ram->hardware_error_status & (1 << 5))
+        fprintf(fout, "    - Bit 5: Overload Error\n");
+    if (ram->hardware_error_status & (1 << 6))
+        fprintf(fout, "    - Bit 6: Unknown??\n");
+    if (ram->hardware_error_status & (1 << 7))
+        fprintf(fout, "    - Bit 7: Unknown??\n");
+
+    fprintf(fout, "  Goal PWM (100):\t\t%.3f%% (%d)\n", ram->goal_pwm * 0.113, ram->goal_pwm);
+    fprintf(fout, "  Goal Velocity (104):\t\t%.3f rpm (%ld)\n", ram->goal_velocity * 0.229, ram->goal_velocity);
+    fprintf(fout, "  Goal Position (116):\t\t%ld\n", ram->goal_position);
+    fprintf(fout, "  Present PWM (124):\t\t%.3f%% (%d)\n", ram->present_pwm * 0.113, ram->present_pwm);
+    fprintf(fout, "  Present Load (126):\t\t%d.%d%%\n", ram->present_load / 10, ram->present_load % 10);
+    fprintf(fout, "  Present Velocity (128):\t%.3f rpm (%ld)\n", ram->present_velocity * 0.229, ram->present_velocity);
+    fprintf(fout, "  Present Position (132):\t%ld\n", ram->present_position);
+    fprintf(fout, "  Present Voltage (144):\t%d.%d V\n", ram->present_input_voltage / 10,
+            ram->present_input_voltage % 10);
+    fprintf(fout, "  Present Temperature (146):\t%d C\n", ram->present_temperature);
     fprintf(fout, "-------------------\n");
     return 0;
 }
+
+static const char *const baudrates[] = { "9,600b", "57,600b", "115,200b", "1Mb", "2Mb", "3Mb", "4Mb", "4.5Mb" };
+static const char *const operating_modes[] = {
+    "Unknown", "Velocity Control", "Unknown", "Position Control", "Extended Position Control",
+    "Unknown", "Unknown",          "Unknown", "Unknown",          "Unknown",
+    "Unknown", "Unknown",          "Unknown", "Unknown",          "Unknown",
+    "Unknown", "PWM Control"
+};
 
 static int dyneeprom_cmd_cb(size_t argc, const char *const *argv, FILE *fout) {
     if (argc < 2) {
@@ -140,26 +182,38 @@ static int dyneeprom_cmd_cb(size_t argc, const char *const *argv, FILE *fout) {
     }
 
     fprintf(fout, "--- Servo %d EEPROM ---\n", id);
-    fprintf(fout, "  Model Number: %d\n", eeprom->model_num);
-    fprintf(fout, "  Model Info: %ld\n", eeprom->model_info);
-    fprintf(fout, "  Firmware Version: %d\n", eeprom->firmware_version);
-    fprintf(fout, "  ID: %d\n", eeprom->id);
-    fprintf(fout, "  Baud Rate: %d\n", eeprom->baud_rate);
-    fprintf(fout, "  Return Delay Time: %d\n", eeprom->return_delay_time);
-    fprintf(fout, "  Drive Mode: %d\n", eeprom->drive_mode);
-    fprintf(fout, "  Operating Mode: %d\n", eeprom->operating_mode);
-    fprintf(fout, "  Secondary ID: %d\n", eeprom->secondary_id);
-    fprintf(fout, "  Protocol: %d\n", eeprom->protocol_type);
-    fprintf(fout, "  Homing Offset: %ld\n", eeprom->homing_offset);
-    fprintf(fout, "  Temperature Limit: %d\n", eeprom->temperature_limit);
-    fprintf(fout, "  Min Voltage Limit: %d\n", eeprom->min_voltage_limit);
-    fprintf(fout, "  Max Voltage Limit: %d\n", eeprom->max_voltage_limit);
-    fprintf(fout, "  PWM Limit: %d\n", eeprom->pwm_limit);
-    fprintf(fout, "  Velocity Limit: %ld\n", eeprom->velocity_limit);
-    fprintf(fout, "  Min Position Limit: %ld\n", eeprom->min_position_limit);
-    fprintf(fout, "  Max Position Limit: %ld\n", eeprom->max_position_limit);
-    fprintf(fout, "  Startup Config: %d\n", eeprom->startup_config);
-    fprintf(fout, "-------------------\n");
+    fprintf(fout, "  Model Number (0):\t\t%d\n", eeprom->model_num);
+    fprintf(fout, "  Model Info (2):\t\t%ld\n", eeprom->model_info);
+    fprintf(fout, "  Firmware Version (6):\t\tv%d\n", eeprom->firmware_version);
+    fprintf(fout, "  ID (7):\t\t\t%d\n", eeprom->id);
+    fprintf(fout, "  Baud Rate (8):\t\t%s (%d)\n", str_table_lookup(baudrates, eeprom->baud_rate), eeprom->baud_rate);
+    fprintf(fout, "  Return Delay Time (9):\t%d us\n", eeprom->return_delay_time);
+    fprintf(fout, "  Drive Mode (10):\t\t%d\n", eeprom->drive_mode);
+    fprintf(fout, "  Operating Mode (11):\t\t%s (%d)\n", str_table_lookup(operating_modes, eeprom->operating_mode),
+            eeprom->operating_mode);
+    fprintf(fout, "   - Bit 0: Servo Direction:\t%s\n",
+            (eeprom->operating_mode & (1 << 0) ? "Normal (CCW+, CW-)" : "Reverse (CW+, CCW-)"));
+    fprintf(fout, "   - Bit 2: Profile Config:\t%s\n",
+            (eeprom->operating_mode & (1 << 2) ? "Velocity-based" : "Time-based"));
+    fprintf(fout, "   - Bit 3: Torque Auto-Enable:\t%s\n", bool_lookup(eeprom->operating_mode & (1 << 3)));
+    fprintf(fout, "  Secondary ID (12):\t\t%d\n", eeprom->secondary_id);
+    fprintf(fout, "  Protocol (13):\t\tv%d\n", eeprom->protocol_type);
+    fprintf(fout, "  Homing Offset (20):\t\t%ld\n", eeprom->homing_offset);
+    fprintf(fout, "  Moving Threshold (24):\t%.3f rpm (%ld)\n", eeprom->moving_threshold * 0.229,
+            eeprom->moving_threshold);
+    fprintf(fout, "  Temperature Limit (31):\t%d C\n", eeprom->temperature_limit);
+    fprintf(fout, "  Min Voltage Limit (32):\t%d.%d V\n", eeprom->min_voltage_limit / 10,
+            eeprom->min_voltage_limit % 10);
+    fprintf(fout, "  Max Voltage Limit (34):\t%d.%d V\n", eeprom->max_voltage_limit / 10,
+            eeprom->max_voltage_limit % 10);
+    fprintf(fout, "  PWM Limit (36):\t\t%.3f%% (%d)\n", eeprom->pwm_limit * 0.113, eeprom->pwm_limit);
+    fprintf(fout, "  Velocity Limit (44):\t\t%.3f rpm (%ld)\n", eeprom->velocity_limit * 0.229, eeprom->velocity_limit);
+    fprintf(fout, "  Min Position Limit (48):\t%ld\n", eeprom->min_position_limit);
+    fprintf(fout, "  Max Position Limit (49):\t%ld\n", eeprom->max_position_limit);
+    fprintf(fout, "  Startup Config (60):\t\t%d\n", eeprom->startup_config);
+    fprintf(fout, "   - Bit 0: Startup Torque On:\t%s\n", bool_lookup(eeprom->startup_config & (1 << 0)));
+    fprintf(fout, "   - Bit 1: RAM Restore:\t%s\n", bool_lookup(eeprom->startup_config & (1 << 1)));
+    fprintf(fout, "-----------------------\n");
     return 0;
 }
 
