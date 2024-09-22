0cbca15ff84c9ee076b32b0d76402a5c01d08560
Noah Charlton <ncharlton002@gmail.com>
2024-09-22 13:58:05 -0400

    Code fixes

--- SBCBoard/src/bq25730.c
+++ SBCBoard/src/bq25730.c
@@ -6,6 +6,7 @@
 #include "bq25730.h"
 
 #include "driver/async_i2c.h"
+#include <stdio.h>
 
 #define BQ25730_I2C_TOTAL_REGISTERS_SIZE 64
 
@@ -18,15 +19,7 @@ static bool i2c_request_in_progress = false;
 
 typedef enum bq25730_register {
     BQ25730_REG_CHARGER_STATUS = 0x20,
-    BQ25730_REG_PROCHOT_STATUS = 0x22,
-    BQ25730_REG_IIN_DPM = 0x24,
     BQ25730_REG_MANUFACTURER_ID = 0x2E,
-    BQ25730_REG_OTG_VOLTAGE = 0x2E,
-    BQ25730_REG_MANUFACTURER_ID = 0x2E,
-    BQ25730_REG_MANUFACTURER_ID = 0x2E,
-    BQ25730_REG_MANUFACTURER_ID = 0x2E,
-    BQ25730_REG_MANUFACTURER_ID = 0x2E
-
     // .. //
 } bq25730_register_t;
 
@@ -40,13 +33,17 @@ static void bq25730_read_data(bq25730_register_t reg) {
     async_i2c_enqueue(&i2c_req, &i2c_request_in_progress);
 }
 
-static void bq25730_write_data(bq25730_register_t reg_start, uint8_t *data, size_t data_len) {}
-
-static void bq25730_on_i2c_failure(const struct async_i2c_request *, uint32_t) {
-    printf("oh oh\n");
+static void bq25730_write_data(bq25730_register_t reg_start, uint8_t *data, size_t data_len) {
+    (void) reg_start;
+    (void) data;
+    (void) data_len;
 }
 
-static void bq25730_on_i2c_req_complete(const struct async_i2c_request *, uint32_t) {
+static void bq25730_on_i2c_failure(const struct async_i2c_request *req, uint32_t error) {
+    printf("Error: %lu\n", error);
+}
+
+static void bq25730_on_i2c_req_complete(const struct async_i2c_request *req) {
     printf("Manufacturer ID: %x\n", i2c_rx_buffer[0]);
 }
 
@@ -55,8 +52,8 @@ void bq25730_init(unsigned int busNum) {
     i2c_req.address = 0x6b;
     i2c_req.tx_buffer = i2c_tx_buffer;
     i2c_req.rx_buffer = i2c_rx_buffer;
-    i2c_req.bytes_to_receive = NULL;
-    i2c_req.bytes_to_send = NULL;
+    i2c_req.bytes_to_receive = 0;
+    i2c_req.bytes_to_send = 0;
     i2c_req.completed_callback = bq25730_on_i2c_req_complete;
     i2c_req.failed_callback = bq25730_on_i2c_failure;
     i2c_req.next_req_on_success = NULL;
