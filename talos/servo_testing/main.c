2b9cf483f2da8e470f19177461573bd6c22bdb3a
Effo12345 <4p7septipuses@gmail.com>
2025-01-17 22:09:06 -0500

    Untested servo rx

--- talos/servo_testing/main.c
+++ talos/servo_testing/main.c
@@ -2,6 +2,7 @@
 
 #include "hardware/watchdog.h"
 #include "pico/stdlib.h"
+#include "titan/logger.h"
 #include "titan/version.h"
 
 #include <stdio.h>
@@ -14,6 +15,8 @@
 #define HEADER_SIZE 2
 #define HEADER_CODE 0x55
 #define CHECKSUM_SIZE 1
+
+#define MAX_PACKET_SIZE 10
 
 #define READ_ID_PIN 0
 #define SET_ID_PIN 1
@@ -28,7 +31,9 @@ typedef struct UartPacket {
     uint8_t checksum;
 } UartPacket_t;
 
-static void on_packet_sent(enum async_uart_tx_err error) {}
+static UartPacket_t packet;
+uint8_t raw_packet[MAX_PACKET_SIZE];
+static bool is_read_request = false;
 
 uint8_t calculate_checksum(UartPacket_t *packet) {
     uint data_sum = packet->target_id + packet->command_length + packet->command;
@@ -39,20 +44,63 @@ uint8_t calculate_checksum(UartPacket_t *packet) {
     return (uint8_t) (~data_sum);
 }
 
-void send_packet(UartPacket_t *packet) {
-    uint8_t output[packet->command_length + HEADER_SIZE + CHECKSUM_SIZE];
-    output[0] = output[1] = HEADER_CODE;
-    output[2] = packet->target_id;
-    output[3] = packet->command_length;
-    output[4] = packet->command;
+// This function is terrifyingly unsafe.
+// TODO: rewrite this better since you actually get the data array in (duh)
+static void on_packet_received(enum async_uart_rx_err error, uint8_t *data, size_t len) {
+    UartPacket_t rx_packet = { .target_id = raw_packet[2], .command_length = raw_packet[3], .command = raw_packet[4] };
 
-    for (uint8_t i = 0; i < packet->command_length - 3; i++) {
-        output[i + 5] = packet->param_buf[i];
+    // Do at least *some* error checking before trying to read into the void
+    if (rx_packet.command_length + 3 > MAX_PACKET_SIZE || rx_packet.command_length < 3) {
+        LOG_ERROR("Received an improperly-sized packet: length %u\n", rx_packet.command_length + 3);
+        return;
     }
 
-    output[sizeof(output) / sizeof(uint8_t) - 1] = calculate_checksum(packet);
+    for (uint8_t i = 0; i < rx_packet.command_length - 3; i++) {
+        rx_packet.param_buf[i] = raw_packet[i + 5];
+    }
+    rx_packet.checksum = raw_packet[rx_packet.command_length + HEADER_SIZE + CHECKSUM_SIZE - 1];
 
-    async_uart_write(output, sizeof(output) / sizeof(uint8_t), false, on_packet_sent);
+    // Make sure everything matches
+    if (rx_packet.target_id != packet.target_id) {
+        LOG_WARN("Response packet source (%x) didn't match target (%x)\n", rx_packet.target_id, packet.target_id);
+        // This can happen in some commands, so don't return early
+    }
+    else if (calculate_checksum(&rx_packet) != rx_packet.checksum) {
+        LOG_ERROR("Received checksum (%x) didn't match expected (%x\n", rx_packet.checksum,
+                  calculate_checksum(&rx_packet));
+        return;  // This should set off a retransmit either here or in higher logic
+    }
+
+    // Packet received correctly, make it available
+    packet = rx_packet;
+
+    LOG_INFO("Packet data: ");
+    for (uint8_t i = 0; i < PARAMETER_MTU; i++)
+        LOG_INFO("%x ", packet.param_buf[i]);
+    LOG_INFO("\n");
+}
+
+static void on_packet_sent(enum async_uart_tx_err error) {
+    if (is_read_request) {
+        async_uart_read(raw_packet, packet.command_length + HEADER_SIZE + CHECKSUM_SIZE, on_packet_received);
+    }
+}
+
+void send_packet() {
+    uint packet_size = packet.command_length + HEADER_SIZE + CHECKSUM_SIZE;
+
+    raw_packet[0] = raw_packet[1] = HEADER_CODE;
+    raw_packet[2] = packet.target_id;
+    raw_packet[3] = packet.command_length;
+    raw_packet[4] = packet.command;
+
+    for (uint8_t i = 0; i < packet.command_length - 3; i++) {
+        raw_packet[i + 5] = packet.param_buf[i];
+    }
+
+    raw_packet[packet_size - 1] = calculate_checksum(&packet);
+
+    async_uart_write(raw_packet, packet_size, false, on_packet_sent);
 }
 
 void split_uint(uint val, uint8_t *out_lsb, uint8_t *out_msb) {
