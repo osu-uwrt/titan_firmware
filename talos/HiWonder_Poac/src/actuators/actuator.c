e9ddb69ba10a99a97e2c921204887ffc810ba69e
Effo12345 <4p7septipuses@gmail.com>
2025-02-16 20:53:54 -0500

    Change to Poac TODO: Integrate power features

--- talos/HiWonder_Poac/src/actuators/actuator.c
+++ talos/HiWonder_Poac/src/actuators/actuator.c
@@ -230,8 +230,8 @@ void servo_set_deg_then_home(float deg) {
 }
 
 void init_servo() {
-    gpio_disable_pulls(CLAW_CHECK_PIN);
-    async_uart_init(pio0, 0, CLAW_CHECK_PIN, UART_BAUD, UART_TIMEOUT_MS);
+    gpio_disable_pulls(DYNAMIXEL_PWM_PIN);
+    async_uart_init(pio0, 0, DYNAMIXEL_PWM_PIN, UART_BAUD, UART_TIMEOUT_MS);
 
     servo_go_home();
 }
