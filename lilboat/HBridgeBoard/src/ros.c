09bbff744f6fc94046ffc1af2b7397c1054eaed6
Effo12345 <4p7septipuses@gmail.com>
2025-05-26 03:06:56 -0400

    Updated bridge mapping (THIS IS BAD)

--- lilboat/HBridgeBoard/src/ros.c
+++ lilboat/HBridgeBoard/src/ros.c
@@ -30,9 +30,9 @@
 #define HBRIDGE_SUBSCRIBER_NAME_TEMPLATE "hbridge_board/command/bridge%u_percent"
 
 const char HBRIDGE_SUBSCRIBER_NAMES[][30] = { [MOTOR_PORT_BRIDGE_NUM] = "command/motor/port",
-                                              [MOTOR_STDB_BRIDGE_NUM] = "command/motor/stdb",
+                                              [MOTOR_STDB_BRIDGE_NUM] = "command/motor/stbd",
                                               [THRUSTER_PORT_BRIDGE_NUM] = "command/thruster/port",
-                                              [THRUSTER_STDB_BRIDGE_NUM] = "command/thruster/stdb",
+                                              [THRUSTER_STDB_BRIDGE_NUM] = "command/thruster/stbd",
                                               [EXTRA_BRIDGE_NUM] = "command/bridge2/unused" };
 
 bool ros_connected = false;
